676d0a9e07988c178321d386233d0bfb9df44ca4
andres.viorels99 <andres.viorels99@gmail.com>
2019-09-28 00:26:24 -0400

    creacion correcta de webapi

---
+++ WebEstudiante/WebEstudiante/Models/DataContext.cs
@@ -0,0 +1,15 @@
+﻿
+namespace WebEstudiante.Models
+{
+    using System.Data.Entity;
+
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<WebEstudiante.Models.Student> Students { get; set; }
+    }
+}
---
+++ WebEstudiante/WebEstudiante/Models/Student.cs
@@ -0,0 +1,34 @@
+﻿
+
+namespace WebEstudiante.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    public class Student
+
+    {
+        [Key]
+        public int FriendID { get; set; }
+        [Required]
+        [Display(Description ="Nombre")]
+        [StringLength(50, MinimumLength = 5)]
+        public string Name { get; set; }
+        [Display(Description ="Lista de nombres")]
+        public Lista list { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress,ErrorMessage ="Direccion de correo no valida")]
+        public string Email { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime BirthDate{ get; set; }
+
+
+    }
+    public enum Lista {
+        Valeria,
+        Vasco,
+        Valentin,
+        Virginia,
+        Virgilio
+    }
+}
